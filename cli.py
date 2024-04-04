53415d0fe7e96ab73d602b92ccd9a46559527dbe
kylephan <kyledphan@gmail.com>
2024-04-04 11:24:43 +0100

    cli.py yuh

--- cli.py
+++ cli.py
@@ -1,72 +1,123 @@
 import typer
 import jsonschema
 import json
-from rich.progress import track
+from typing import Optional
 
 app = typer.Typer()
-        
-def get_id():
+
+@app.command()
+def get_id_and_name():
+    with open("schema.json") as schema_file:
+                schema_name = json.load(schema_file)["title"]
     try:
         with open("db.json") as file:
-            return json.load(file)[-1]["id"]
+            return json.load(file)[-1]["id"] + 1, schema_name
     except:
-        return 1
+        return 1, schema_name
 
+@app.command()
 def validate(data):
     with open("schema.json") as file:
         schema = json.load(file)
     try:
         jsonschema.validate(data, schema)
         print("JSON data is valid.")
+        return True
     except jsonschema.exceptions.ValidationError as e:
         print(f"JSON data is invalid: {e.message}")
+        return False
 
 @app.command()
-def create(data):
-    id = get_id()
+def create(data: str):
+    data = json.loads(data)
+    id, schema_name = get_id_and_name()
     data["id"] = id
-    validate(data)
+    if not validate(data):
+        exit()
+
     try:
-        with open("db.json", 'r') as file:
-            db = json.load(file)
-            db.append(data)
+        with open("db.json", 'r+') as file:
+            file_data = json.load(file)
+            file_data.append(data)
+            file.seek(0)
+            json.dump(file_data, file, indent=4)   
     except FileNotFoundError:
         pass
     
 @app.command()
+def create_database():
+
+    data = []
+    db_file = open('db.json', 'w')
+    json.dump(data, db_file)
+
+@app.command()
 def read(all:bool = False, id:int = False):
     if all:
-       with open("db.json") as file:
-           db = json.load(file)
-           print(db)
+        with open('db.json', 'r') as file:
+            db = json.load(file)
+            print(db)
     elif id:
-        with open("db.json") as file:
-           db = json.load(file)
-        print(db[id - 1])
+        with open('db.json', 'r') as file:
+            db = json.load(file)
+        print(db[id-1])
     else:
         print("ERROR")
 
 @app.command()
-def search(value:str = typer.Option(), keyword:str = False, update:int = False):
+def search(value:str = typer.Option(), keyword: str=None, update:int=0):
     with open("db.json") as file:
         db = json.load(file)
-        #NEED TO TEST
-        if update:
-            for record in db:
-                if record["id"] == update:
-                    record["id"] = json.loads(value)
-                    break
-            file.seek(0)
+    if update:
+        for record in db:
+            if record["id"] == update:
+                record = json.loads(value)
+                record["id"] = update
+                if not validate(record):
+                    exit()
+                db[update-1] = record
+                break
+
+        with open('db.json', 'w') as file:
             json.dump(db, file, indent=4)
-        else:
-            for record in db:
-                if keyword:
-                    if record[keyword] == value or (type(record[keyword]) == list and value in record[keyword]):
+    else:
+        for record in db:
+            if keyword:
+                if record[keyword] == value or (type(record[keyword]) == list and value in record[keyword]):
+                    print(record)
+            else:
+                for val in record.values():
+                    if val == value or (type(val) == list and value in val):
                         print(record)
-                else:
-                    for val in record.values():
-                        if val == value or (type(val) == list and value in val):
-                            print(record)
+            
+@app.command()
+def delete(id:int = False, all:bool = False):
+    if all:
+        try:
+            with open("db.json", 'r+') as file:
+                file_data = json.load(file)
+                file_data = []
+                file.seek(0)
+                open('db.json', 'w').close()
+                json.dump(file_data, file, indent=4)
+        except FileNotFoundError:
+            pass
+    elif id:
+        try:
+            with open("db.json", 'r+') as file:
+                file_data = json.load(file)
+                for key, item in enumerate(file_data):
+                    if item['id'] == id:
+                        file_data = file_data[:key] + file_data[key+1:]
+                        print(file_data)
+                        break
+                file.seek(0)
+                open('db.json', 'w').close()
+                json.dump(file_data, file, indent=4)   
+        except FileNotFoundError:
+            pass
+    else:
+        print("ERROR")
 
 if __name__ == '__main__':
     app()
